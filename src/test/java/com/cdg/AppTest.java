b92e73bd502d55e97f27f920bbe291f18a600a67
sanghun <2002lkw@naver.com>
2022-08-30 22:20:38 +0900

    junit-jupiter 로통일

--- src/test/java/com/cdg/AppTest.java
+++ src/test/java/com/cdg/AppTest.java
@@ -1,8 +1,8 @@
 package com.cdg;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
